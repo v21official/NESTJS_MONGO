9cb2345cea46071b9a84f69d240b352b3687f737
vinhnk <vinhnk245@gmail.com>
2021-01-09 09:36:32 +0700

    public login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,40 +1,17 @@
-import { BadRequestException, Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Request, SetMetadata, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+export const Public = () => SetMetadata("isPublic", true);
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
+    @Public()
     @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Request() req) {
         return this.authService.login(req.user);
     }
-
-    // @Post('register')
-    // async register(@Body() body) {
-    //     try {
-    //         return await this.authService.register(body);
-    //     } catch (error) {
-    //         throw new BadRequestException(error.message || error.toString());
-    //     }
-    // }
-
-    // @UseGuards(JwtAuthGuard)
-    // @Get('profile')
-    // getProfile(@Request() req) {
-    //     return req.user;
-    // }
-
-    // @UseGuards(JwtAuthGuard)
-    // @Post('changePassword')
-    // async changePassword(@Request() req, @Body() body) {
-    //     try {
-    //         return await this.authService.changePassword({ user: req.user, body });
-    //     } catch (error) {
-    //         throw new BadRequestException(error.message || error.toString());
-    //     }
-    // }
 }
